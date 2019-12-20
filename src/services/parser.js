178f125145ff58dd6917552b3e2417607c183c2c
Adam Bantly <24380+yadam@users.noreply.github.com>
2019-12-20 10:59:47 -0800

    Step 4: Deny negative numbers by throwing an exception that includes all of the negative numbers provided

--- src/services/parser.js
+++ src/services/parser.js
@@ -1,10 +1,20 @@
 export function parse(input) {
   let result = 0;
+  const badOperands = [];
   const operands = input.split(/[,\n]/);
   operands.forEach(o => {
     const operand = parseInt(o) || 0;
+    if (operand < 0) {
+      badOperands.push(operand);
+      return;
+    }
     result += operand;
   });
+  if (badOperands.length > 0) {
+    throw new Error(
+      `Negative numbers not allowed - [ ${badOperands.join(', ')} ]`,
+    );
+  }
   return result;
 }
 
